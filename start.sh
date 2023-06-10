fb01bc164b437257b441beabdcbb0a35f1646654
Ankith SN <97029263+notankith@users.noreply.github.com>
2023-06-10 14:41:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Judit
 fi
-cd /PiroAutoFilterBot
+cd /Judith
 pip3 install -U -r requirements.txt
 echo "Starting Judith...."
 python3 bot.py
