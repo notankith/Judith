21be0b5b9ada51c867da52a817d6398fa79b7515
Ankith SN <97029263+notankith@users.noreply.github.com>
2023-06-10 14:18:19 +0530

    🍾💥

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/notankith/judith.git /Judith
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Judit
 fi
 cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Judith...."
 python3 bot.py
